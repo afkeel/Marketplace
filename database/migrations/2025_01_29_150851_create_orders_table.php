9b992b1fbde76c7cd442df5d4d1d2115204b12b4
ArtemF <blademasterii@rambler.ru>
2025-01-30 16:18:47 +0500

    тип данных номера заказа генерируется как uuid

--- database/migrations/2025_01_29_150851_create_orders_table.php
+++ database/migrations/2025_01_29_150851_create_orders_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
                 ->constrained() 
                 ->onUpdate('restrict')
                 ->onDelete('cascade');
-            $table->unsignedInteger('number')->unique();
+            $table->unsignedInteger('uuid')->unique();
             $table->dateTime('order_date');
             $table->timestamps();
 
